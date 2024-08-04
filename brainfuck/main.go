bde8344c8abe51750cd4ab7f8c878c6fcbc91389
e-aub <eaub@proton.me>
2024-08-04 10:35:08 +0100

    handled errors

--- brainfuck/main.go
+++ brainfuck/main.go
@@ -12,41 +12,59 @@ func main() {
 		fmt.Fprintln(os.Stderr, "The program accepts only one argument")
 		return
 	}
-	commands := os.Args[1]
+	sourceCode := os.Args[1]
 	Indices = make(map[int]int)
-	indices(commands)
-	parse(commands)
+	indices(sourceCode)
+	parse(sourceCode)
 }
 
-func parse(commands string) {
+func parse(sourceCode string) {
+	var result string
 	ptr := 0
 	bytes := make([]byte, 2048)
-	for i := 0; i < len(commands); i++ {
-		if commands[i] == '>' {
+	for i := 0; i < len(sourceCode); i++ {
+		if sourceCode[i] == '>' {
+			if ptr == 2047 {
+				fmt.Fprintf(os.Stderr, "pointer out of range [%d] with length %d\n", i+1, 2048)
+				return
+			}
 			ptr++
-		} else if commands[i] == '<' {
+		} else if sourceCode[i] == '<' {
+			if ptr == 0 {
+				fmt.Fprintln(os.Stderr, "pointer out of range [-1]")
+				return
+			}
 			ptr--
-		} else if commands[i] == '+' {
+		} else if sourceCode[i] == '+' {
+			if bytes[ptr] == 255 {
+				fmt.Fprintf(os.Stderr, "byte overflow at pointer %d\n", ptr)
+				return
+			}
 			bytes[ptr]++
-		} else if commands[i] == '-' {
+		} else if sourceCode[i] == '-' {
+			if bytes[ptr] == 0 {
+				fmt.Fprintf(os.Stderr, "byte underflow at pointer %d\n", ptr)
+				return
+			}
 			bytes[ptr]--
-		} else if commands[i] == '[' {
+		} else if sourceCode[i] == '[' {
 			if bytes[ptr] == 0 {
 				i = Indices[i]
 			}
-		} else if commands[i] == ']' {
+		} else if sourceCode[i] == ']' {
 			if bytes[ptr] != 0 {
 				i = Indices[i]
 			}
-		} else if commands[i] == '.' {
-			fmt.Print(string(bytes[ptr]))
+		} else if sourceCode[i] == '.' {
+			result += string(bytes[ptr])
 		}
 	}
+	fmt.Print(result)
 }
 
-func indices(commands string) {
+func indices(sourceCode string) {
 	stack := []int{}
-	for i, char := range commands {
+	for i, char := range sourceCode {
 		if char == '[' {
 			stack = append(stack, i)
 		} else if char == ']' {
